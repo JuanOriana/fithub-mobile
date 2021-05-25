8f114d85d8fce4635e775188d48526a444b94154
Juan Oriana <joriana@itba.edu.ar>
2021-05-25 17:16:44 -0300

    Routine activity

--- app/src/main/java/com/example/fithub_mobile/RoutineCard.java
+++ app/src/main/java/com/example/fithub_mobile/RoutineCard.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class RoutineCard extends MaterialCardView {
 
@@ -14,6 +17,12 @@ public class RoutineCard extends MaterialCardView {
         inflate(getContext(),R.layout.routine_card,this);
         TextView text = findViewById(R.id.title_card);
         text.setText(title);
+
+        MaterialButton routineBtn = findViewById(R.id.routine_btn);
+        routineBtn.setOnClickListener(view -> {
+            Intent i = new Intent(getContext(),RoutineActivity.class);
+            getContext().startActivity(i);
+        });
     }
 
 }
