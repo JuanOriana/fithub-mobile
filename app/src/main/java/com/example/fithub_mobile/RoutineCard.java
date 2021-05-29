f78e4ae0b46f8d2ea4bdf0bf5c5453655d96f24a
Juan Oriana <joriana@itba.edu.ar>
2021-05-29 15:10:33 -0300

    Sharing working

--- app/src/main/java/com/example/fithub_mobile/RoutineCard.java
+++ app/src/main/java/com/example/fithub_mobile/RoutineCard.java
@@ -3,8 +3,10 @@ package com.example.fithub_mobile;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -19,6 +21,7 @@ public class RoutineCard extends MaterialCardView {
     public static String TITLE_MESSAGE = "com.example.fithub_mobile.NAME";
     public static final String RATING_MESSAGE = "com.example.fithub_mobile.RATING";
     public static final String DESC_MESSAGE = "com.example.fithub_mobile.DESC";
+    public static final String SHARE_MESSAGE = "com.example.fithub_mobile.SHARE";
     public String title;
 
 
@@ -34,9 +37,20 @@ public class RoutineCard extends MaterialCardView {
         RatingBar ratingBar = findViewById(R.id.rating_bar);
         ratingBar.setRating(rating);
 
+        ImageButton shareBtn = findViewById(R.id.share_btn);
+        shareBtn.setOnClickListener(view -> {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.fithub);
+            String shareMessage= "\n"+ getContext().getString(R.string.share_msg)+"\n\n";
+            shareMessage = shareMessage + "http://fithub.com/routine" +"\n\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            getContext().startActivity(Intent.createChooser(shareIntent, "Choose one"));
+        });
+
         MaterialButton routineBtn = findViewById(R.id.routine_btn);
         routineBtn.setOnClickListener(view -> {
-            Intent i=new Intent(Intent.ACTION_VIEW);
+            Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData(Uri.parse("http://fithub.com/routine"));
             i.setPackage("com.example.fithub_mobile");
             getContext().startActivity(i);
@@ -46,6 +60,7 @@ public class RoutineCard extends MaterialCardView {
 //            i.putExtra(DESC_MESSAGE, desc);
 //            getContext().startActivity(i);
         });
+
     }
 
     public String getTitle(){
