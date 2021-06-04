981036a467e70b6a6d29486e0db19247a492b00a
federojas <frojas@itba.edu.ar>
2021-06-04 13:16:35 -0300

    queue_execution progress

--- app/src/main/java/com/example/fithub_mobile/ExecutionActivity.java
+++ app/src/main/java/com/example/fithub_mobile/ExecutionActivity.java
@@ -1,8 +1,12 @@
 package com.example.fithub_mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ExecutionActivity extends AppCompatActivity {
 
@@ -10,7 +14,14 @@ public class ExecutionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_execution);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.execution_menu, menu);
+        return true;
+    }
 
 }
--- app/src/main/java/com/example/fithub_mobile/ExecutionQueueActivity.java
+++ app/src/main/java/com/example/fithub_mobile/ExecutionQueueActivity.java
@@ -1,8 +1,18 @@
 package com.example.fithub_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fithub_mobile.ui.home.HomeViewModel;
 
 public class ExecutionQueueActivity extends AppCompatActivity {
 
@@ -10,5 +20,12 @@ public class ExecutionQueueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_execution_queue);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.execution_menu, menu);
+        return true;
     }
 }
