7a136401f558d74cc1078dea75bf5c4e2c57c4fb
Juan Oriana <joriana@itba.edu.ar>
2021-06-09 16:58:50 -0300

    IDEK what is going on

--- app/src/main/java/com/example/fithub_mobile/NotificationActivity.java
+++ app/src/main/java/com/example/fithub_mobile/NotificationActivity.java
@@ -13,13 +13,20 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 
 public class NotificationActivity extends AppCompatActivity {
 
     static final private String CHANNEL_ID = "Routine Notif";
     public static final int REQUEST_CODE_NOTIFY = 1;
+    private CheckBox[] days;
+
 
 
     @Override
@@ -36,21 +43,36 @@ public class NotificationActivity extends AppCompatActivity {
 
         Button cancelBtn = findViewById(R.id.cancel_btn_notif);
         Button acceptBtn = findViewById(R.id.accept_btn);
+        TimePicker tp = findViewById(R.id.notif_tp);
+        days = new CheckBox[7];
+        days[0] = findViewById(R.id.checkBox_mon);
+        days[1] = findViewById(R.id.checkBox_tue);
+        days[2] = findViewById(R.id.checkBox_wed);
+        days[3] = findViewById(R.id.checkBox_thu);
+        days[4] = findViewById(R.id.checkBox_fri);
+        days[5] = findViewById(R.id.checkBox_sat);
+        days[6] = findViewById(R.id.checkBox_sun);
 
         acceptBtn.setOnClickListener(v->{
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            calendar.set(Calendar.HOUR_OF_DAY, tp.getHour());
+            calendar.set(Calendar.MINUTE, tp.getMinute());
+
             AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-            long currentTimeMs = SystemClock.elapsedRealtime();
 
             PendingIntent pendingNotifyIntent = PendingIntent.getBroadcast(
                     this,
                     42,
                     new Intent( this, NotifyHandlerReceiver.class ),
-            PendingIntent.FLAG_UPDATE_CURRENT);
-            alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, currentTimeMs + 100000, pendingNotifyIntent);
+            0);
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    AlarmManager.INTERVAL_DAY, pendingNotifyIntent);
         });
 
         cancelBtn.setOnClickListener(v->{
             finish();
         });
     }
+
 }
