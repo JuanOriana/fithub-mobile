9659f8a3c40373b53fb8ae1c377fdc6b8530f6c1
Juan Oriana <joriana@itba.edu.ar>
2021-06-10 09:57:10 -0300

    Back end models

---
+++ app/src/main/java/com/example/fithub_mobile/backend/models/FullCycleExercise.java
@@ -0,0 +1,89 @@
+
+package com.example.fithub_mobile.backend.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class FullCycleExercise {
+
+    @SerializedName("exercise")
+    @Expose
+    private FullExercise exercise;
+    @SerializedName("order")
+    @Expose
+    private int order;
+    @SerializedName("duration")
+    @Expose
+    private int duration;
+    @SerializedName("repetitions")
+    @Expose
+    private int repetitions;
+    @SerializedName("metadata")
+    @Expose
+    private Object metadata;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public FullCycleExercise() {
+    }
+
+    /**
+     *
+     * @param duration
+     * @param metadata
+     * @param exercise
+     * @param repetitions
+     * @param order
+     */
+    public FullCycleExercise(FullExercise exercise, int order, int duration, int repetitions, Object metadata) {
+        super();
+        this.exercise = exercise;
+        this.order = order;
+        this.duration = duration;
+        this.repetitions = repetitions;
+        this.metadata = metadata;
+    }
+
+    public FullExercise getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(FullExercise exercise) {
+        this.exercise = exercise;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getRepetitions() {
+        return repetitions;
+    }
+
+    public void setRepetitions(int repetitions) {
+        this.repetitions = repetitions;
+    }
+
+    public Object getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Object metadata) {
+        this.metadata = metadata;
+    }
+
+}
