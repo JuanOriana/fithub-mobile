5e2b21effa2944e0e0e1933dfef706f884bfeac1
Juan Oriana <joriana@itba.edu.ar>
2021-06-11 19:01:19 -0300

    Queue management nice

--- app/src/main/java/com/example/fithub_mobile/ExecutionQueueActivity.java
+++ app/src/main/java/com/example/fithub_mobile/ExecutionQueueActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.fithub_mobile.excercise.ExerciseData;
@@ -31,52 +33,61 @@ public class ExecutionQueueActivity extends AppCompatActivity {
     ExerciseData currentExercise;
     RecyclerView exerciseContainer;
     ExerciseAdapter adapter;
+    ProgressBar pgBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_execution_queue);
 
-        exercises.add(new ExerciseData(1,"Diácono","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
-        exercises.add(new ExerciseData(1,"Diácono","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
-        exercises.add(new ExerciseData(1,"Diácono","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
-        exercises.add(new ExerciseData(1,"Diácono","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
-        exercises.add(new ExerciseData(1,"Diácono","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
+        exercises.add(new ExerciseData(1,"Diácono1","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
+        exercises.add(new ExerciseData(1,"Diácono2","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
+        exercises.add(new ExerciseData(1,"Diácono3","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
+        exercises.add(new ExerciseData(1,"Diácono4","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
+        exercises.add(new ExerciseData(1,"Diácono5","Prueba",4,4,"http://i.imgur.com/DvpvklR.png"));
 
         exerciseContainer = findViewById(R.id.exercise_container);
         exerciseContainer.setLayoutManager(new LinearLayoutManager(this));
         adapter = new ExerciseAdapter(exercises);
         exerciseContainer.setAdapter(adapter);
 
+        pgBar = findViewById(R.id.progressBarQueue);
+        pgBar.setProgress(0);
+
         setNextExercise();
 
         ImageButton nextBtn = findViewById(R.id.next_queue);
         nextBtn.setOnClickListener(view -> setNextExercise());
+        ImageButton prevBtn = findViewById(R.id.prev_queue);
+        prevBtn.setOnClickListener(view -> setPrevExercise());
 
     }
 
     private void setNextExercise(){
-        if (exercises.size() == 0)
+        if (exercises.size() == 0) {
             finish();
+            return;
+        }
         if (currentExercise != null){
             doneExercises.add(currentExercise);
         }
         currentExercise = exercises.remove(0);
         setCurrentInfo(currentExercise);
         adapter.notifyDataSetChanged();
+        updateProgress();
     }
 
 
-//    private void setPrevExercise(){
-//        if (exercises.size() == 0)
-//            finish();
-//        if (currentExercise != null){
-//            doneExercises.add(currentExercise);
-//        }
-//        currentExercise = exercises.remove(0);
-//        setCurrentInfo(currentExercise);
-//        adapter.notifyDataSetChanged();
-//    }
+    private void setPrevExercise(){
+        if (doneExercises.size() == 0)
+            return;
+        exercises.add(0,currentExercise);
+        currentExercise = doneExercises.remove(doneExercises.size()-1);
+        setCurrentInfo(currentExercise);
+        adapter.notifyDataSetChanged();
+        updateProgress();
+    }
+
     private void setCurrentInfo(ExerciseData currentExercise){
         View current = this.findViewById(R.id.current_exercise_card);
 
@@ -91,6 +102,15 @@ public class ExecutionQueueActivity extends AppCompatActivity {
 
         ImageView currentImage = current.findViewById(R.id.current_image);
         Picasso.get().load("https://i.imgur.com/DvpvklR.png").into(currentImage);
+    }
+
+    private void updateProgress(){
+        if (pgBar == null)
+            return;
+        int nextLen = exercises.size();
+        int doneLen = doneExercises.size();
+        double ratio = doneLen*1.0/(nextLen+doneLen);
+        pgBar.setProgress((int)(ratio*100));
     }
 
     @Override
